12eb3c777249012f493b1d77062ceb27d1e43aef
Shlomi Steinberg <p@shlomisteinberg.com>
2019-11-11 19:36:32 -0800

    Config

--- compile.sh
+++ compile.sh
@@ -1,1 +1,1 @@
-gcc -lm -std=gnu++2a -Icolor-util/include -I/usr/include/eigen3 image_diff.cpp -o image_diff -lpthread -lpng -lstdc++ 
+gcc -std=gnu++2a -Icolor-util/include -I/usr/include/eigen3 image_diff.cpp -o image_diff -lpthread -lpng -lm -lstdc++
